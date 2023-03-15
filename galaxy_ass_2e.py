5e4aa9158e7414d07f3aa985dcbb1e5dc6a30588
Texas-Alex <82863901+Texas-Alex@users.noreply.github.com>
2023-03-15 11:44:58 +0800

    Commenting

--- galaxy_ass_2e.py
+++ galaxy_ass_2e.py
@@ -4,7 +4,8 @@ import numpy as np
 import math
 import statistics as stats
 
-
+#PARAM filename: filename of the galaxy to be read
+#RETURN: np.arrays for the right ascention, cz velocity and luminosity of each galaxy in the cluster
 def readgalaxy (filename):
     try:
         ra, cz,lum = np.genfromtxt(filename, unpack = True, usecols = (0,4,-1),dtype = float, delimiter = ',')
@@ -22,16 +23,16 @@ def main():
     HUBBLE = 67.77
     max_lum = 0.0
     count = 0
-    print(lum[0])
 
     if key == "coma":
         DISTANCE = 103.198 # in Mpc
         for i in range(len(ra)):
-            if cz[i]/HUBBLE <=DISTANCE+1 and cz[i]/HUBBLE >= DISTANCE-1:
+            if cz[i]/HUBBLE <=DISTANCE+1 and cz[i]/HUBBLE >= DISTANCE-1:  #check if the galaxy is withing 1Mpc of the centre
                 count +=1
                 if lum[i] >  max_lum:
                     max_lum = lum[i]
-        S_mass = max_lum/3
+                    
+        S_mass = max_lum/3 #determine stellar mass of the brightest galaxy using the formula in 2e
 
         print("stellar mass of brightest galaxy in " + key + " cluster is "+ str(S_mass))
 
@@ -39,11 +40,12 @@ def main():
     if key == "abell":
         DISTANCE = 335.578 # in Mpc
         for i in range(len(ra)):
-            if cz[i]/HUBBLE <=DISTANCE+1 and cz[i]/HUBBLE >= DISTANCE-1:
+            if cz[i]/HUBBLE <=DISTANCE+1 and cz[i]/HUBBLE >= DISTANCE-1: #check if the galaxy is within 1Mpc of the centre
                 count += 1
                 if lum[i] >  max_lum:
                     max_lum = lum[i]
-        S_mass = max_lum/3
+                    
+        S_mass = max_lum/3 #determine stellar mass of the brightest galaxy using the formula in 2e
 
         print("stellar mass of brightest galaxy in " + key + " cluster is " + str(S_mass))
 
