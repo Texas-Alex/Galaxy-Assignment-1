3ab5d123dc31f3308aaed58c585c152bac4c5a3c
Texas-Alex <82863901+Texas-Alex@users.noreply.github.com>
2023-03-15 11:24:10 +0800

    Adding clarification comments

--- galaxy_ass_2b.py
+++ galaxy_ass_2b.py
@@ -5,6 +5,7 @@ import statistics as stat
 
 
 #function to separate the ra, dec and cz from the rest of the file 
+# the raw files used still contain a header that is removed when reading the file
 def readgalaxy (filename):
     try:
         ra, dec, cz = np.genfromtxt(filename, unpack = True, usecols = (0,1,4),dtype = float, delimiter = ',')
@@ -22,8 +23,12 @@ def main():
     
     ra, dec, cz = readgalaxy(galaxyfile)
 
+    
+    #read the file line by line and determine if it is close enough within the cluster
+    #checks which cluster to ensure the right values are used for ra and dec along with writing the correctly named filtered_ file
     with open(galaxyfile, "r") as galf:
         lines = galf.readlines()
+        
         if key == 'abell':
             for i in range(len(lines)-1):
                 if (ra[i] >= 257.6 and ra[i]<= 258.6) and (dec[i] >= 63.5 and dec[i] <=64.5 )and (cz[i] >= 18000 and cz[i] <=30000):
