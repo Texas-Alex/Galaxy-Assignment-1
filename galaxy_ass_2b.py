0b9275ee52377a184523096e98f3973e49d09660
Texas-Alex <82863901+Texas-Alex@users.noreply.github.com>
2023-03-17 12:47:04 +0800

    Updated 2b
    Searches for distance to centre of the cluster rather than guessing RA and DEC

--- galaxy_ass_2b.py
+++ galaxy_ass_2b.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 import sys
 import numpy as np
+import math
 import statistics as stat
 
 
 #function to separate the ra, dec and cz from the rest of the file 
-#the raw files used still contain a header that is removed when reading the file
-#PARAM filename: name of the file to bea read
-#RETRUN: header removed np.arrays containing the right ascention, declination and cz of every galaxy in the cluster 
 def readgalaxy (filename):
     try:
         ra, dec, cz = np.genfromtxt(filename, unpack = True, usecols = (0,1,4),dtype = float, delimiter = ',')
@@ -25,15 +23,12 @@ def main():
     
     ra, dec, cz = readgalaxy(galaxyfile)
 
-    
-    #read the file line by line and determine if it is close enough within the cluster
-    #checks which cluster to ensure the right values are used for ra and dec along with writing the correctly named filtered_ file
     with open(galaxyfile, "r") as galf:
         lines = galf.readlines()
-        
         if key == 'abell':
             for i in range(len(lines)-1):
-                if (ra[i] >= 257.6 and ra[i]<= 258.6) and (dec[i] >= 63.5 and dec[i] <=64.5 )and (cz[i] >= 18000 and cz[i] <=30000):
+                dist = math.sqrt((ra[i]-258.1292)**2 + (dec[i]-64.0925)**2)
+                if (dist <= 5 and (cz[i] >= 18000 and cz[i] <=30000)):
                     valid_gals.append(lines[i])
                 
             with open("filtered_a.csv", "w") as newfile:
@@ -42,7 +37,8 @@ def main():
 
         if key  == 'coma':
             for i in range(len(lines)-1):
-                if (ra[i] >= 194. and ra[i] <= 195.4) and (dec[i] >= 27.2 and dec[i] <= 28.3) and (cz[i] >= 4500 and cz[i] <= 9250):
+                dist = math.sqrt((ra[i]-194.95799)**2 + (dec[i]-27.98300)**2)
+                if (dist <= 5 and (cz[i] >= 4500 and cz[i] <= 9250)):
                     valid_gals.append(lines[i])
                 
             with open("filtered_c.csv", "w") as newfile:
